32c7876f3d48aabcf52730f1d29750123c95269c
kat8335 <carreno.katerina@gmail.com>
2022-10-28 23:41:21 +0200

    showing the current temperature

---
+++ My-weatherApp-project/src/app.js
@@ -0,0 +1,21 @@
+function displayTemperature(response) {
+  
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+  cityElement.innerHTML = response.data.city;
+  descriptionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = response.data.temperature.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+
+}
+
+
+let apiKey= "3423f9e30e80437oae320f8639950tb8";
+let query = "Florence";
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${query}&key=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(displayTemperature);
