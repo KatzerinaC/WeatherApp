52826a3e312b91ebb02f08e120a07e8e2290c582
kat8335 <carreno.katerina@gmail.com>
2022-10-29 00:17:36 +0200

    updated the time

--- My-weatherApp-project/src/app.js
+++ My-weatherApp-project/src/app.js
@@ -1,3 +1,18 @@
+function formatDate(timeStamp){
+  let date = new Date(timeStamp);
+  let hours = date.getHours();
+  if (hours < 10 ) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+  let day = days[date.getDay()];
+return `${day} ${hours}:${minutes}`;
+}
+
 function displayTemperature(response) {
   
   let temperatureElement = document.querySelector("#temperature");
@@ -5,11 +20,14 @@ function displayTemperature(response) {
   let descriptionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
+  let dateElement = document.querySelector("#date");
   temperatureElement.innerHTML = Math.round(response.data.temperature.current);
   cityElement.innerHTML = response.data.city;
   descriptionElement.innerHTML = response.data.condition.description;
   humidityElement.innerHTML = response.data.temperature.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
+  dateElement.innerHTML = formatDate(response.data.time);
+  
 
 }
 
